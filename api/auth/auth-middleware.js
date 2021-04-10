9a37f47b8adfe1c110c744459e228ec5f324db25
ChadDiaz <chadcdiaz@gmail.com>
2021-04-10 15:23:36 -0400

    working with joon

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const jwt = require("jsonwebtoken");
+const User = require("../users/users-model.js");
 
 const restricted = (req, res, next) => {
   const token = req.headers?.authorization?.split(" ")[1];
@@ -34,7 +35,14 @@ const restricted = (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
 const only = (role_name) => (req, res, next) => {
-  /*
+  const realRole = req.decodedToken?.role;
+  if (realRole === role_name) {
+    next();
+  } else {
+    res.status(403).json({ message: "this is not for you" });
+  }
+};
+/*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
     status 403
@@ -44,20 +52,44 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-};
 
-const checkUsernameExists = (req, res, next) => {
-  /*
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    const user = await User.findBy({ username: req.body.username });
+    if (user.length > 0) {
+      next();
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+/*
     If the username in req.body does NOT exist in the database
     status 401
     {
       "message": "Invalid credentials"
     }
   */
-};
 
-const validateRoleName = (req, res, next) => {
-  /*
+const validateRoleName = async (req, res, next) => {
+  let desiredRole = req.body.role_name;
+  if (desiredRole) {
+    if (desiredRole.trim() === "admin") {
+      res.status(422).json({ message: "role can not be admin" });
+    } else if (desiredRole.trim().length > 32) {
+      res
+        .status(422)
+        .json({ message: "Role name can not be longer than 32 characters" });
+    }
+ 
+  } else {
+    req.body.role_name = "student";
+    next();
+  }
+};
+/*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
     If role_name is missing from req.body, or if after trimming it is just an empty string,
@@ -75,7 +107,6 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-};
 
 module.exports = {
   restricted,
